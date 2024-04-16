70cd21eac34ba350120680229f39163befc8e29d
piconina <lorenaterenzi@gmail.com>
2024-04-16 20:01:04 -0300

    criação footer

--- recicla-mais-codigo/recicla-mais/src/components/layout/Footer.js
+++ recicla-mais-codigo/recicla-mais/src/components/layout/Footer.js
@@ -1,7 +1,33 @@
-import './Footer.css'
+import "./Footer.css";
+import PBH from "../../imgs/PrefeituraBH.png";
+import Instagram from "../../imgs/instagram.png";
+import xIcon from "../../imgs/x.png";
+import YouTube from "../../imgs/youtube.png";
+import FaleConosco from "../../imgs/fale-conosco.png"
 
-function Footer(){
-    return
+function Footer() {
+  return (
+    <div className="container_footer">
+      <img src={PBH} alt="PrefeituraBH" id='pbh'/>
+      <div className="container_icones">
+        <div className="icones">
+          <a href="">
+            <img src={Instagram} alt="instagram" />
+          </a>
+          <a href="">
+            <img src={xIcon} alt="x" />
+          </a>
+          <a href="">
+            <img src={YouTube} alt="youtube" />
+          </a>
+        </div>
+        <span>
+          <p id='copyright'>Copyright &copy;2023 ReciclaMais</p>
+        </span>
+      </div>
+      <a href=''><img src={FaleConosco} alt='fale-conosco' id='fale_conosco'/></a>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
