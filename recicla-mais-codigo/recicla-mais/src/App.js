28ab02f23cea9e5f717680ad1f3f2b4f78f8588e
piconina <lorenaterenzi@gmail.com>
2024-04-16 23:01:40 -0300

    criação agendamento

--- recicla-mais-codigo/recicla-mais/src/App.js
+++ recicla-mais-codigo/recicla-mais/src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import TemplateSPA from './pages/TemplateSPA';
+import Agendamento from "./pages/Agendamento";
 
 function App() {
   return (
     <>
       <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TemplateSPA/>} />
+        <Route path="/" element={<Agendamento/>} />
       </Routes>
       </BrowserRouter>
     </>
---
+++ recicla-mais-codigo/recicla-mais/src/components/textBox/BoxEndereco.js
@@ -0,0 +1,18 @@
+import './BoxEndereco.css'
+
+function BoxEndereco() {
+  return (
+    <div className="divEndereco">
+      <div className="divEndereco_titulo">
+        <h3>Endereço:</h3>
+      </div>
+      <div className="divEndereco_endereco">
+        <p>
+          R. Endereço de Exemplo, XXXX - Bairro, Belo Horizonte - MG, XXXXX-XXX
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default BoxEndereco;
