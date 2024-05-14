85e8d6d59962edb00e097eb878c5dbf3d89fcc3f
piconina <lorenaterenzi@gmail.com>
2024-05-13 21:27:12 -0300

    criação do botaoRedM e alteração do esqueleto do perfil

--- recicla-mais-codigo/recicla-mais/src/components/buttons/BotaoVerdeP.js
+++
@@ -1,7 +0,0 @@
-import './BotaoVerdeP.css';
-
-function BotaoVerdeP({evento, texto}) {
-    return (<button className='botaoVerdeP' onClick={evento}>{texto}</button>)
-}
-
-export default BotaoVerdeP
--- recicla-mais-codigo/recicla-mais/src/pages/Perfil.js
+++ recicla-mais-codigo/recicla-mais/src/pages/Perfil.js
@@ -5,8 +5,10 @@ import CardPontuacaoPerfil from "../components/perfil/CardPontuacaoPerfil";
 import BotaoTrocarPontos from "../components/perfil/BotaoTrocarPontos";
 import TituloAzul from "../components/perfil/TituloAzul";
 import DivInfos from "../components/perfil/DivInfos";
-import BotaoVerdeP from "../components/buttons/BotaoVerdeP";
+import BotaoVerdeM from "../components/buttons/BotaoVerdeM";
 import TabelaAzul from "../components/tabelas/TabelaAzul";
+
+import BotaoRedM from "../components/buttons/BotaoRedM";
 
 function Perfil() {
   const headersAgendamento = ["Data:", "Horário:", "Endereço:", "Itens:"];
@@ -22,12 +24,15 @@ function Perfil() {
           <TituloAzul titulo="Dados:" />
           <DivInfos />
           <div className="divBotaoAlterar">
-            <BotaoVerdeP texto="Alterar dados" />
+            <BotaoVerdeM texto="Alterar dados" />
           </div>
         </div>
         <div className="divAgendamentoPerfil">
           <TituloAzul titulo="Próximo agendamento:" />
           <TabelaAzul headersTabela={headersAgendamento} />
+          <div className="divBotaoCancelarAgendamento">
+            <BotaoRedM texto='Cancelar agendamento' />
+          </div>
         </div>
       </section>
     </main>
